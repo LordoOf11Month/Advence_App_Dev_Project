93d90d3434614034078ea808d8d1e1ba84f15bc4
Ramazan Secilmis <ramazan.secilmis15@gmail.com>
2025-05-10 03:02:30 +0300

    data initializer is functional but schema creted by entities because until the end database should be dinamic

--- e-commerce-backend/src/main/java/com/example/controllers/admin/AdminProductController.java
+++ e-commerce-backend/src/main/java/com/example/controllers/admin/AdminProductController.java
@@ -5,8 +5,6 @@ import com.example.DTO.ProductDTO.ProductResponse;
 import com.example.DTO.ProductDTO.CreateProductRequest;
 import com.example.models.Product;
 import com.example.services.ProductService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,17 +18,5 @@ public class AdminProductController extends GenericController<Product, ProductRe
     public AdminProductController(ProductService productService) {
         super(productService);
         this.productService = productService;
-    }
-
-    @PostMapping
-    public ResponseEntity<ProductResponse> addProduct(@RequestBody CreateProductRequest createProductRequest) {
-        ProductResponse productResponse = productService.save(createProductRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.delete(id);
-        return ResponseEntity.noContent().build();
     }
 } 
